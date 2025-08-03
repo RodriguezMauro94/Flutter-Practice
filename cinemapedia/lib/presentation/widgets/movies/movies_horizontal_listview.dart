294594b38bf53defa7800bd5bb4db4e04b428a78
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2025-08-03 18:52:02 -0300

    added intl

--- cinemapedia/lib/presentation/widgets/movies/movies_horizontal_listview.dart
+++ cinemapedia/lib/presentation/widgets/movies/movies_horizontal_listview.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_format.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -113,19 +114,25 @@ class _Slide extends StatelessWidget {
             child: Text(movie.title, maxLines: 2, style: textTheme.titleSmall),
           ),
 
-          Row(
-            children: [
-              Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
-              const SizedBox(width: 3),
-              Text(
-                movie.voteAverage.toString(),
-                style: textTheme.bodyMedium?.copyWith(
-                  color: Colors.yellow.shade800,
+          SizedBox(
+            width: 150,
+            child: Row(
+              children: [
+                Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
+                const SizedBox(width: 3),
+                Text(
+                  movie.voteAverage.toString(),
+                  style: textTheme.bodyMedium?.copyWith(
+                    color: Colors.yellow.shade800,
+                  ),
                 ),
-              ),
-              const SizedBox(width: 10),
-              Text(movie.popularity.toString(), style: textTheme.bodySmall),
-            ],
+                Spacer(),
+                Text(
+                  HumanFormat.number(movie.popularity),
+                  style: textTheme.bodySmall,
+                ),
+              ],
+            ),
           ),
         ],
       ),
