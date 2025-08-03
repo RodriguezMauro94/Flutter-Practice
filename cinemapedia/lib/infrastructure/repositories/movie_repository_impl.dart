1d39674d1c19d20899449e8177237a805d2cf59b
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2025-08-03 11:34:17 -0300

    implement movie repository

---
+++ cinemapedia/lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,14 @@
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/respositories/movies_repository.dart';
+
+class MovieRepositoryImpl extends MoviesRepository {
+  final MoviesDatasource moviesDatasource;
+
+  MovieRepositoryImpl({required this.moviesDatasource});
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return moviesDatasource.getNowPlaying(page: page);
+  }
+}
